34214308529953c70b3f26e9c22ade04cea7e11a
etrix06 <alc18005@byui.edu>
2021-09-19 21:44:23 -0700

    changes made from academind video

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
-const http = require('http');
+//const http = require('http');
+// we can get rid of above code because express listen does it for us.
+const express = require('express');
 
-const routes = require('./routes');
+const app = express();  //initializes a new object in the constant app
 
-console.log(routes.someText);
+app.use('/',(req, res, next) => {
+  console.log('This always runs!');
+  next();
+});
 
-const server = http.createServer(routes.handler);
+app.use('/add-product',(req, res, next) => {
+  console.log('In another middleware');
+  res.send('<h1>The add product page</h1>');
+});
 
-server.listen(3000);
+app.use('/',(req, res, next) => {
+  console.log('In another middleware');
+  res.send('<h1>Hello from Express!</h1>');
+});
+
+//const server = http.createServer(app);
+//server.listen(3000);
+// instead of above use listen
+app.listen(3000);
+
+console.log('listening on 3000');
