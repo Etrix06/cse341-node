b92126924fae2e60393b7ef9220bbc520285bed3
etrix06 <alc18005@byui.edu>
2021-09-20 22:29:51 -0700

    ch

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
-//const http = require('http');
-// we can get rid of above code because express listen does it for us.
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();  //initializes a new object in the constant app
 
-app.use('/',(req, res, next) => {
-  console.log('This always runs!');
-  next();
-});
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/add-product',(req, res, next) => {
-  console.log('In another middleware');
-  res.send('<h1>The add product page</h1>');
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
+
+app.post('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 
 app.use('/',(req, res, next) => {
-  console.log('In another middleware');
   res.send('<h1>Hello from Express!</h1>');
 });
 
