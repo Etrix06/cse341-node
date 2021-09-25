e7283eb012f24774c7bb3d0d55d37d5c52321a74
etrix06 <alc18005@byui.edu>
2021-09-25 13:27:17 -0700

    updated files

--- app.js
+++ app.js
@@ -1,26 +1,22 @@
+const path = require('path');
 const express = require('express');
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');  //called on terminal using npm install --save body-parser
 
 const app = express();  //initializes a new object in the constant app
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-product',(req, res, next) => {
-  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
-app.post('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-});
-
-app.use('/',(req, res, next) => {
-  res.send('<h1>Hello from Express!</h1>');
-});
-
-//const server = http.createServer(app);
-//server.listen(3000);
-// instead of above use listen
 app.listen(3000);
 
 console.log('listening on 3000');
